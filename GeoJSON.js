77b522b6b9d0df670cc4a1d76ff51aad2ee4e1cc
unknown <lmiller36@wisc.edu>
2018-09-11 19:34:53 -0500

    Cleaned up version

--- GeoJSON.js
+++
@@ -1,6 +0,0 @@
-function initializeGeoJSON(){
-    var US_States = "http://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_outline_500k.json";
-    getJSON(US_States,(arr)=>{
-        console.log(arr);
-    });
-}
